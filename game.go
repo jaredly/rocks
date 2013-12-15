858c0c45bab1eb6078d0ee3c4acadb622e654c28
Jared Forsyth <jared@jaredforsyth.com>
2013-12-15 01:41:50 -0700

    awesome spinny

---
+++ game.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	gl "github.com/chsc/gogl/gl21"
+  "math/rand"
+  // "fmt"
+)
+
+type Point struct {
+	Color int
+	Intensity int
+}
+
+const (
+  WHITE = 1
+  RED = 2
+  GREEN = 3
+  BLUE = 4
+)
+
+type Color struct {
+  R gl.Float
+  G gl.Float
+  B gl.Float
+  A gl.Float
+}
+
+func UseColor(c Color) {
+  gl.Color4f(c.R, c.G, c.B, c.A)
+}
+
+func PointColor(p Point) Color {
+  n := gl.Float(float64(p.Intensity) / 10.0)
+  if p.Color == WHITE {
+    return Color{1.0, 1.0, 1.0, 1}
+  }
+  if p.Color == RED {
+    return Color{n, 0, 0, 1}
+  }
+  if p.Color == GREEN {
+    return Color{0, n, 0, 1}
+  }
+  return Color{0, 0, n, 1}
+}
+
+func Dir() (int, int) {
+  n := rand.Intn(8)
+  d := [][2]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}[n]
+  return d[0], d[1]
+}
+
+func Win(p *Point) {
+  if p.Intensity >= 10 {
+    return
+  }
+  p.Intensity += 1
+}
+
+func Lose(p *Point, ncolor int) {
+  if p.Intensity > 1 {
+    p.Intensity -= 1
+    return
+  }
+  p.Intensity = 1
+  p.Color = ncolor
+}
+
+func Beats(c1, c2 int) bool {
+  if c1 == RED {
+    return c2 == GREEN
+  }
+  if c1 == BLUE {
+    return c2 == RED
+  }
+  return c2 == BLUE
+}
+
+func StepOne(x, y, width, height int, matrix *[][]Point) {
+  dx, dy := Dir()
+  x2 := x + dx
+  y2 := y + dy
+  if x2 < 0 || x2 >= width {
+    return
+  }
+  if y2 < 0 || y2 >= height {
+    return
+  }
+  p1 := &(*matrix)[x][y]
+  p2 := &(*matrix)[x2][y2]
+  if p1.Color == p2.Color {
+    return
+  }
+  if p1.Color == WHITE {
+    if p2.Intensity < 2 {
+      return
+    }
+    p1.Intensity = p2.Intensity - 1
+    p1.Color = p2.Color
+    return
+  }
+  if p2.Color == WHITE {
+    if p1.Intensity < 2 {
+      return
+    }
+    p2.Intensity = p1.Intensity - 1
+    p2.Color = p1.Color
+    return
+  }
+  if Beats(p2.Color, p1.Color) {
+    Win(p2)
+    Lose(p1, p2.Color)
+  } else {
+    Win(p1)
+    Lose(p2, p2.Color)
+  }
+}
+
+func StepMatrix(width, height int, matrix *[][]Point) {
+  for x := range *matrix {
+    for y := range (*matrix)[x] {
+      StepOne(x, y, width, height, matrix)
+    }
+  }
+}
+
+func makeMatrix(width, height int) *[][]Point {
+  m := make([][]Point, width)
+  for x := range m {
+    m[x] = make([]Point, height)
+    for y := range m[x] {
+      m[x][y].Color = WHITE
+      m[x][y].Intensity = 10
+    }
+  }
+  a := width/3
+  drawStripe(a, a, 20, 20, &m, RED)
+  drawStripe(a+20, a, 20, 20, &m, BLUE)
+  drawStripe(a, a+20, 20, 20, &m, GREEN)
+  drawStripe(a+20, a+20, 20, 20, &m, RED)
+  a += 40
+  drawStripe(a, a, 20, 20, &m, RED)
+  drawStripe(a+20, a, 20, 20, &m, BLUE)
+  drawStripe(a, a+20, 20, 20, &m, GREEN)
+  drawStripe(a+20, a+20, 20, 20, &m, RED)
+
+  drawStripe(a, a-20, 20, 20, &m, GREEN)
+  drawStripe(a-20, a, 20, 20, &m, BLUE)
+  return &m
+}
+
+func drawStripe(x, y, w, h int, m *[][]Point, c int) {
+  for a := x; a < x + w; a++ {
+    for b := y; b < y + h; b++ {
+      (*m)[a][b].Color = c
+      (*m)[a][b].Intensity = 10
+    }
+  }
+}
+
+func drawMatrix(m *[][]Point, xby, yby gl.Float) {
+  for x, row := range *m {
+    for y, p := range row {
+      UseColor(PointColor(p))
+      // fmt.Println(p, PointColor(p))
+      // gl.Color4f(1, 0, 0, 1)
+      gl.Vertex2f(gl.Float(x) * xby, gl.Float(y) * yby)
+    }
+  }
+}
+
---
+++ gopher.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	// "bytes"
+	// "errors"
+	"fmt"
+	gl "github.com/chsc/gogl/gl21"
+	"github.com/jteeuwen/glfw"
+	// "image"
+	// "image/png"
+	// "io"
+	"os"
+)
+
+const (
+	Title  = "Spinning Gopher"
+	Width  = 640
+	Height = 480
+)
+
+var (
+	matrix     *[][]Point
+	texture    gl.Uint
+	rotx, roty gl.Float
+	ambient    []gl.Float = []gl.Float{0.5, 0.5, 0.5, 1}
+	diffuse    []gl.Float = []gl.Float{1, 1, 1, 1}
+	lightpos   []gl.Float = []gl.Float{-5, 5, 10, 0}
+)
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "glfw: %s\n", err)
+		return
+	}
+	defer glfw.Terminate()
+
+	glfw.OpenWindowHint(glfw.WindowNoResize, 1)
+
+	if err := glfw.OpenWindow(Width, Height, 0, 0, 0, 0, 16, 0, glfw.Windowed); err != nil {
+		fmt.Fprintf(os.Stderr, "glfw: %s\n", err)
+		return
+	}
+	defer glfw.CloseWindow()
+
+	glfw.SetSwapInterval(1)
+	glfw.SetWindowTitle(Title)
+
+	if err := gl.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "gl: %s\n", err)
+	}
+
+	if err := initScene(); err != nil {
+		fmt.Fprintf(os.Stderr, "init: %s\n", err)
+		return
+	}
+	defer destroyScene()
+
+	for glfw.WindowParam(glfw.Opened) == 1 {
+		StepMatrix(Width/2, Height/2, matrix)
+		drawScene()
+		glfw.SwapBuffers()
+	}
+}
+
+func initScene() (err error) {
+	gl.Disable(gl.DEPTH_TEST)
+
+	gl.ClearColor(0.5, 0.5, 0.5, 0.0)
+
+	gl.Viewport(0, 0, Width, Height)
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadIdentity()
+	gl.Ortho(0, Width, Height, 0, 0, 1)
+	gl.MatrixMode(gl.MODELVIEW)
+
+	// gl.Enable(gl.POINT_SPRITE) // GL_POINT_SPRITE_ARB if you're
+	// using the functionality as an extension.
+
+	// gl.Enable(gl.POINT_SMOOTH)
+	// gl.Enable(gl.BLEND)
+	// gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+	gl.PointSize(2.0)
+
+	/* assuming you have setup a 32-bit RGBA texture with a legal name */
+	// gl.ActiveTexture(gl.TEXTURE0)
+	// gl.Enable(gl.TEXTURE_2D)
+	// gl.TexEnv(gl.POINT_SPRITE, gl.COORD_REPLACE, gl.TRUE);
+	// gl.TexEnv(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.REPLACE);
+	// gl.BindTexture(gl.TEXTURE_2D, texture)
+
+	x := 0
+	y := 0
+	dcolor := WHITE
+	glfw.SetMouseButtonCallback(func (button, state int) {
+		dcolor += 1
+		if dcolor > BLUE {
+			dcolor = WHITE
+		}
+	})
+	glfw.SetMousePosCallback(func (mx, my int) {
+		x = mx
+		y = my
+		if matrix != nil {
+			(*matrix)[x][y].Color = dcolor
+			(*matrix)[x][y].Intensity = 10
+		}
+	})
+
+	matrix = makeMatrix(Width/2, Height/2)
+
+	return
+}
+
+func destroyScene() {
+	gl.DeleteTextures(1, &texture)
+}
+
+func drawScene() {
+	// gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	gl.Begin(gl.POINTS)
+	drawMatrix(matrix, 2, 2)
+	gl.End()
+	gl.Finish()
+}
